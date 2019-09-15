e8fd533445111c7e8447819cc16419ef38685179
Jon Mrowczynski <jonmrowczynski@gmail.com>
2019-09-15 01:04:41 -0400

    Renamed USBMidiConnection class to better reflect Java class naming conventions.

--- PTSymmetryControllerSoftware/src/ptsymmetrycontroller/connections/UsbMidiConnection.java
+++ PTSymmetryControllerSoftware/src/ptsymmetrycontroller/connections/UsbMidiConnection.java
@@ -10,62 +10,61 @@ import javax.sound.midi.ShortMessage;
 import javax.sound.midi.MidiUnavailableException;
 
 /**
- * Allows for the creation of a USB to MIDI connection in order to send constructed {@code MidiMessage}s
- * to the microcontroller. Specifically a USB {@code MidiDevice} is acquired, along with its {@code Receiver}.
- * <p>
- * {@code MidiMessage}s are sent to the microcontroller in order to pulse the {@code Solenoid}s.
- * 
+ * A singleton class that can connect to a USB {@code MidiDevice} in order to send {@code MidiMessage}s to the
+ * microcontroller to pulse the {@code Solenoid}s. This is accomplished by sending those {@code MidiMessage}s directly
+ * to the acquired {@code Receiver} of the USB {@code MidiDevice}.
+ *
  * @author Jon Mrowczynski
- * @version 2.0
  */
 
-public final class USBMidiConnection {
+public final class UsbMidiConnection {
 	
 	/**
-	 * The singleton instance object of this class. There should only ever be one USB to MIDI connection.
+	 * The singleton instance object of this class.
 	 */
 	
-	private static final USBMidiConnection usbMidiConnection = new USBMidiConnection();
+	private static final UsbMidiConnection usbMidiConnection = new UsbMidiConnection();
 		
 	/**
-	 * Returns the singleton instance of this class.
+	 * Returns the singleton instance.
 	 * 
-	 * @return the singleton {@code USBMidiConnection} instance of this class.
+	 * @return the singleton {@code UsbMidiConnection} instance.
 	 */
 	
-	public static USBMidiConnection getInstance() { return usbMidiConnection; }
+	public static UsbMidiConnection getInstance() { return usbMidiConnection; }
 	
 	/**
-	 * No external instances of a {@code USBMidiConnection} should be made.
+	 * No external instances of a {@code UsbMidiConnection} should be made.
 	 */
 	
-	private USBMidiConnection() { }
+	private UsbMidiConnection() { }
 	
 	/**
-	 * The USB {@code MidiDevice} that is handling the transmission of MIDI signals to the microcontroller.
+	 * The USB {@code MidiDevice} that is handling the transmission of {@code MidiMessage}s to the microcontroller.
 	 */
 	
 	private MidiDevice usbMidiDevice = null;
 		
 	/**
-	 * The {@code Receiver} of the USB {@code MidiDevice}. The constructed {@code MidiMessage}s are sent to
-	 * this {@code Receiver} in order to send them to the microcontroller.
+	 * The {@code Receiver} of the USB {@code MidiDevice}. The {@code MidiMessage}s are sent to this {@code Receiver}
+	 * such that they are ultimately passed to the microcontroller.
 	 */
 	
 	private Receiver usbMidiReceiver = null;
 		
 	/**
-	 * Establishes a connection to a USB {@code MidiDevice}, opens it up, and gets its {@code Receiver} so 
-	 * that {@code MidiMessage}s can be ultimately sent to the microcontroller.
+	 * Establishes a connection to a USB {@code MidiDevice} by first finding an appropriate USB {@code MidiDevice} to
+	 * connect to. That device is then opened and its {@code Receiver} is acquired so that {@code MidiMessage}s can be
+	 * sent to the microcontroller.
 	 * 
-	 * @return a {@code boolean} representing whether the USB to MIDI connection has been successfully established.
+	 * @return a {@code boolean} representing whether the connection has been successfully established.
 	 */
 	
 	public final boolean establishUSBMidiConnection() { return getUSBMidiDevice() && openUSBMidiDevice() && getUSBMidiDeviceReceiver(); }
 	
 	/**
-	 * Sends the given MIDI {@code ShortMessage} to the USB {@code MidiDevice}'s {@code Receiver} with
-	 * a {@code timeStamp} of -1. This is entirely a convenience method.
+	 * Sends {@code message} to the USB {@code MidiDevice}'s {@code Receiver} with a {@code timeStamp} of -1. This is
+	 * entirely a convenience method.
 	 * 
 	 * @param message that is to be sent to the USB {@code MidiDevice}'s {@code Receiver}.
 	 */
@@ -74,30 +73,29 @@ public final class USBMidiConnection {
 	
 	/**
 	 * Terminates the connections to the USB {@code MidiDevice}.
-	 * <p>
-	 * Note that closing the USB {@code MidiDevice} implicitly closes its associated acquired {@code Receiver} as well.
+	 *
+	 * Closing the USB {@code MidiDevice} implicitly closes its acquired {@code Receiver} as well.
 	 * 
-	 * @return a {@code boolean} representing whether the USB {@code MidiDevice} is closed.
+	 * @return a {@code boolean} representing whether the USB {@code MidiDevice} is or has been successfully closed.
 	 */
 	
 	public final boolean closeUSB() {
-		if (usbMidiDevice == null)
-			return false;
-		if(usbMidiDevice.isOpen()) {
+		if (usbMidiDevice == null) { return false; }
+		if (usbMidiDevice.isOpen()) {
 			usbMidiDevice.close();
+			usbMidiDevice = null;
 			System.out.println("USB MidiDevice has been closed.");
-		} else 
-			System.out.println("USB MidiDevice was already closed.");
+		} else { System.out.println("USB MidiDevice was already closed."); }
 		return !usbMidiDevice.isOpen();
 	}
 	
 	/**
 	 * Gets the USB {@code MidiDevice} that is connected to the computer whose name contains the 
-	 * {@code String} "USB", and whose description is equal to the {@code String} "External MIDI Port".
-	 * Otherwise, no USB {@code MidiDevice} would be suitable to be acquired for the purpose of this program.
-	 * <p>
+	 * {@code String} "USB" and "MIDIOUT". Otherwise, no USB {@code MidiDevice} would be suitable to be acquired for the
+	 * purpose of this program.
+	 *
 	 * If a USB {@code MidiDevice} was acquired, then {@code true} is returned.
-	 * <p>
+	 *
 	 * If no suitable USB {@code MidiDevice} can be found, then {@link #USBMidiDeviceError(String, String)} is called,
 	 * prompting the user if they would like to retry to search for the USB {@code MidiDevice}. If none can be 
 	 * found and the user doesn't want to retry to search for the USB {@code MidiDevice}, then nothing is acquired 
@@ -107,41 +105,41 @@ public final class USBMidiConnection {
 	 */
 	
 	private boolean getUSBMidiDevice() {
-		boolean foundUSBMidiDevice = false;
 		boolean retry = false;
 		do {
-			for (final Info deviceInfo : MidiSystem.getMidiDeviceInfo()) {
-				final boolean isUSBDevice = deviceInfo.getName().contains("USB");
-				final boolean isExternalMidiPort = deviceInfo.getDescription().equals("External Midi Port");
-				if (isUSBDevice && isExternalMidiPort) {
+			for (final Info info : MidiSystem.getMidiDeviceInfo()) {
+				if (info.getName().contains("USB") && info.getName().contains("MIDIOUT")) {
 					try {
-						usbMidiDevice = MidiSystem.getMidiDevice(deviceInfo);
-						foundUSBMidiDevice = true;
-					} catch(MidiUnavailableException ex) { 
+						final MidiDevice tempMidiDevice = MidiSystem.getMidiDevice(info);
+						if (tempMidiDevice.getMaxReceivers() != 0) {
+							usbMidiDevice = tempMidiDevice;
+							break;
+						}
+					} catch (MidiUnavailableException ex) {
 						ex.printStackTrace();
 						retry = USBMidiDeviceError("Error connecting to the USB MIDI device.",
                                 "Make sure that the USB MIDI cable is plugged into the computer before retrying.");
 					}
 				}
 			}
-		} while(retry);
-		return foundUSBMidiDevice;
+		} while (retry);
+		return usbMidiDevice != null;
 	}
 	
 	/**
 	 * Opens the USB {@code MidiDevice} for operation. 
-	 * <p>
-	 * If it was successfully opened, or if it was already opened, then {@code true} is returned. 
-	 * <p>
-	 * If a USB {@code MidiDevice} has not yet been acquired, then {@code false} is returned. 
-	 * <p>
-	 * Otherwise, if a USB {@code MidiDevice} has been acquired, but it could not be open, then 
-	 * {@link #USBMidiDeviceError(String, String)} is called, prompting the user if they would like to retry
-	 * to open the USB {@code MidiDevice}. If the USB {@code MidiDevice} cannot be opened, and the user 
-	 * doesn't want to retry to open the USB {@code MidiDevice} then {@code false} is returned.
 	 *
-	 * @return a {@code boolean} representing whether the USB {@code MidiDevice} was successfully opened
-	 * 		   or is currently open.
+	 * If it was successfully opened, or if it was already opened, then {@code true} is returned. 
+	 *
+	 * If a USB {@code MidiDevice} has not yet been acquired, then {@code false} is returned. 
+	 *
+	 * Otherwise, if a USB {@code MidiDevice} has been acquired, but it could not be open, then 
+	 * {@link #USBMidiDeviceError(String, String)} is called, prompting the user if they would like to retry to open the
+	 * USB {@code MidiDevice}. If the USB {@code MidiDevice} cannot be opened, and the user doesn't want to retry to
+	 * open the USB {@code MidiDevice} then {@code false} is returned.
+	 *
+	 * @return a {@code boolean} representing whether the USB {@code MidiDevice} was successfully opened or is currently
+	 * 		   open.
 	 */
 	
 	private boolean openUSBMidiDevice() {
@@ -157,46 +155,43 @@ public final class USBMidiConnection {
 					usbMidiDevice.open(); 
 					System.out.println("USB MIDI device was opened for use.");
 				}
-				catch(MidiUnavailableException ex) { 
+				catch (MidiUnavailableException ex) {
 					ex.printStackTrace();
 					retry = USBMidiDeviceError("Error opening USB Midi device.",
                             "Make sure to close any programs that may be using the USB device.");
 				}
-			} else
-				System.out.println("USB MIDI device was already open for use.");
-		} while(retry);
+			} else { System.out.println("USB MIDI device was already open for use."); }
+		} while (retry);
 		return usbMidiDevice.isOpen();
 	}
 	
 	/**
 	 * Gets the {@code Receiver} of the acquired USB {@code MidiDevice}.
-	 * <p>
+	 *
 	 * If it has been successfully obtained, or if it already has been obtained, {@code true} is returned.
-	 * <p>
-	 * If a USB {@code MidiDevice} has not yet been acquired, then {@code false} is returned.
-	 * <p>
-	 * If a USB {@code MidiDevice} has been acquired, but has not yet been opened, then {@code false} is 
-	 * returned since a {@code Receiver} cannot be obtained from a {@code MidiDevice} that is not opened.
+	 *
+	 * If a USB {@code MidiDevice} has not yet been acquired, or if a USB {@code MidiDevice} has been acquired, but has
+	 * not yet been opened, then {@code false} is returned.
 	 * 
-	 * @return a {@code boolean} representing whether the {@code Receiver} of the USB {@code MidiDevice} has been successfully acquired.
+	 * @return a {@code boolean} representing whether the {@code Receiver} of the USB {@code MidiDevice} has been
+	 * 		   successfully acquired.
 	 */
 	
 	private boolean getUSBMidiDeviceReceiver() {
 		if (usbMidiDevice == null) {
-			System.err.println("Must acquire a USB MidiDevice first before its Receiver can be obtained.");
+			System.err.println("Must acquire a USB MidiDevice first before its Receiver can be acquired.");
 			return false;
 		} else if (!usbMidiDevice.isOpen()) {
-			System.err.println("Must open the USB MidiDevice first before its Receiver can be obtained.");
+			System.err.println("Must open the USB MidiDevice first before its Receiver can be acquired.");
 			return false;
 		}
 		try {
 			if(usbMidiReceiver == null) {
 				usbMidiReceiver = usbMidiDevice.getReceiver();
-				System.out.println("USB MIDI device's Receiver obtained.\n");
-			} else
-				System.out.println("USB MIDI device's Receiver already obtained.\n");
+				System.out.println("USB MIDI device's Receiver acquired.\n");
+			} else { System.out.println("USB MIDI device's Receiver already acquired.\n"); }
 			return usbMidiReceiver != null;
-		} catch(MidiUnavailableException ex) { ex.printStackTrace(); }
+		} catch (MidiUnavailableException ex) { ex.printStackTrace(); }
 		return false;
 	}
 
@@ -205,10 +200,10 @@ public final class USBMidiConnection {
      * option to retry to open the USB {@code MidiDevice} is also presented to the user.
      *
      * And error usually occurs when the USB {@code MidiDevice} has not yet been plugged into the computer or when
-     * another program has opened the USB {@code MidiDevice} for its own use and has not yet relinquished control of it.
+	 * another program has opened the USB {@code MidiDevice} for its own use and has not yet relinquished control of it.
      *
-     * @param errorMessage that is to be displayed to the user
-     * @param helpfulMessage that is to help the user resolve the error message
+     * @param errorMessage that is to be displayed to the user.
+     * @param helpfulMessage that is to help the user resolve the error message.
      * @return a {@code boolean} representing whether the user wants to retry to open the USB {@code MidiDevice}.
      */
 
@@ -233,9 +228,9 @@ public final class USBMidiConnection {
                     System.out.println("That wasn't one of the choices.\n");
                     break;
             }
-        } while(!(retryChoice.equals("Y") || retryChoice.equals("N")));
+        } while (!(retryChoice.equals("Y") || retryChoice.equals("N")));
         reader.close();
         return retry;
     }
 	
-} // end of class USBMidiConnection
+} // end of class UsbMidiConnection
